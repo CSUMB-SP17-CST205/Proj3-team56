6131b43280e64a336df9da6e6014deef3ebe80f8
Emmanuel Guido <eguido@csumb.edu>
2017-04-24 21:59:22 +0000

    INO WebPages

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def login():
 	return render_template('index.html')
+	
+@app.route('/doubledouble')
+def doubledouble():
+	return render_template('doubledouble.html')
+	
+@app.route('/inofries')
+def INOFries():
+	return render_template('INOFries.html')
 
 if __name__ == '__main__':
     app.run(

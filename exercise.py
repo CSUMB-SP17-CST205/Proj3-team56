71b67407f3b1b5c6cc44b0734bf3c2e3600c4e1e
eguido52 <eguido@csumb.edu>
2017-05-15 13:20:45 -0700

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -1,12 +1,20 @@
 """
 This function calcuates the amount of time it takes to burn a specified amount of calories. 
-It takes in two arguments, which are the amounts of calories you want to calculate the time for
+It takes in two arguments, which are calories and excercise. The calorie parameter is the 
+amount of calories you want to calculate the time for. The excercise parameter determines 
+the time it takes to burn X amount of calories according to the intesity of the excercise. 
+The intesity of an excercise is measured by an MET score given to each excercise, this 
+score is then plugged in to an equation which calculates the amount of calories you would
+per minute based on weight and the MET score of the excercise. Then we divide the calories 
+from the food by the calories burnt per minute and that gives us the time it would take to 
+burn those calories from that food.
 
 Resource:
 https://www.hss.edu/conditions_burning-calories-with-exercise-calculating-estimated-energy-expenditure.asp
 """
 
 def time2Burn(calFromFood, excercise):
+    //Checks whether excercise is biking/swimming/running
     if excercise == "biking":
         MET = 4.0
         
@@ -16,10 +24,13 @@ def time2Burn(calFromFood, excercise):
     elif excercise == "running":
         MET = 8.0
 
+    //Calculates the calories burned per minute
     energyExpenditure = .0175 * MET * 64.5
     
+    //Calculates the time to burn the calories from food
     time = int(calFromFood) / energyExpenditure
     
+    //rounds the time that was calculated
     roundTime = time - int(time)
     
     if roundTime > .49:
